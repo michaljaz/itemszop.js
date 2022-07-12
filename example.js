43a82677553ea2922d7fe3a5a65cb588d2e9f0d5
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-07-12 15:51:08 +0200

    get all info about shop

--- example.js
+++ example.js
@@ -1,14 +1,21 @@
 import ItemSzop from './index.js'
 
-ItemSzop({
-	firebaseConfig: {
-	  apiKey: 'AIzaSyAgqotnCT9d_zcmwrE8mHWiC9JL8r75U-s',
-	  authDomain: 'sklepmc-c7516.firebaseapp.com',
-	  databaseURL: 'https://sklepmc-c7516-default-rtdb.europe-west1.firebasedatabase.app',
-	  projectId: 'sklepmc-c7516',
-	  storageBucket: 'sklepmc-c7516.appspot.com',
-	  messagingSenderId: '889784816765',
-	  appId: '1:889784816765:web:7c84a7bce9b480ce7d4d14'
-	},
-	shopId: 'gitcraft'
-})
+(async () => {
+  const shop = new ItemSzop()
+  await shop.init({
+    firebaseConfig: {
+  	  apiKey: 'AIzaSyAgqotnCT9d_zcmwrE8mHWiC9JL8r75U-s',
+  	  authDomain: 'sklepmc-c7516.firebaseapp.com',
+  	  databaseURL: 'https://sklepmc-c7516-default-rtdb.europe-west1.firebasedatabase.app',
+  	  projectId: 'sklepmc-c7516',
+  	  storageBucket: 'sklepmc-c7516.appspot.com',
+  	  messagingSenderId: '889784816765',
+  	  appId: '1:889784816765:web:7c84a7bce9b480ce7d4d14'
+    },
+    shopId: 'gitcraft'
+  })
+
+  console.log(shop.getServers())
+  console.log(shop.getConfig())
+  console.log(shop.getShop())
+})()
--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 import { initializeApp } from 'firebase/app'
-import { getDatabase, ref, onValue } from 'firebase/database'
+import { getDatabase, ref, get } from 'firebase/database'
 
-export default (data) => {
-	const {firebaseConfig, shopId} = data
-  const app = initializeApp(firebaseConfig)
-  const db = getDatabase(app)
-
-  const shopRef = ref(db, `shops/${shopId}`)
-  onValue(shopRef, (snapshot) => {
-    const data = snapshot.val()
-    console.log(data)
-  })
+class ItemSzop {
+  constructor () {
+    this.shop = {}
+  }
+  async init ({firebaseConfig, shopId}) {
+    const db = getDatabase(initializeApp(firebaseConfig))
+    this.shop = (await get(ref(db, `shops/${shopId}`))).val()
+    this.servers = {}
+    for (const serverId in this.shop.servers) {
+      this.servers[serverId] = (await get(ref(db, `servers/${serverId}/serverName`))).val()
+    }
+    this.config = {
+      microsms_sms_text: (await get(ref(db, `config/${shopId}/microsms_sms_text`))).val(),
+      lvlup: (await get(ref(db, `config/${shopId}/lvlup`))).val(),
+      microsms: (await get(ref(db, `config/${shopId}/microsms`))).val(),
+      paypal: (await get(ref(db, `config/${shopId}/paypal`))).val(),
+      paypal_id: (await get(ref(db, `config/${shopId}/paypal_id`))).val()
+    }
+  }
+  getServers () {
+    return this.servers
+  }
+  getConfig () {
+    return this.config
+  }
+  getShop () {
+    return this.shop
+  }
 }
+
+export default ItemSzop
